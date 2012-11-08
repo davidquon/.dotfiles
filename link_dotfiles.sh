92690c2b1e611f3b30eff94bbd648cc4955381ec
David Quon <davidq@amcoonline.net>
2012-11-07 18:53:43 -0800

    * Added link_dotfiles.sh and moved files

---
+++ link_dotfiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ln -snf ~/.dotfiles/bash_aliases ~/.bash_aliases
+ln -snf ~/.dotfiles/bash_profile ~/.bash_profile
+ln -snf ~/.dotfiles/bashrc ~/.bashrc
+ln -snf ~/.dotfiles/gitignore ~/.gitignore
+ln -snf ~/.dotfiles/vimrc ~/.gvimrc_git
+ln -snf ~/.dotfiles/vim ~/.vim
+ln -snf ~/.dotfiles/vimrc ~/.vimrc
